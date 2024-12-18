9789c803743fbe6ee6820323ccc548dd970ae702
joshuasparkes <joshsparkes6@gmail.com>
2024-12-18 14:22:27 +0000

    add port to search.py

--- search.py
+++ search.py
@@ -19,3 +19,6 @@ def home():
     return jsonify({"message": "Welcome to the backend!"})
 
 # Add more routes as needed
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
